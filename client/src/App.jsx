cae772823e3580270a620c515f96bf9da3681c00
pikkauhn <114180449+pikkauhn@users.noreply.github.com>
2023-10-13 14:52:05 -0500

    ready for testing

---
+++ client/src/App.jsx
@@ -0,0 +1,154 @@
+
+import { useEffect, useState, useRef } from 'react';
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
+import Axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const [isPulledNameVisible, setIsPulledNameVisible] = useState(false);
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [nameEntered, setNameEntered] = useState('');
+  const [pulledName, setPulledName] = useState('');
+  const [data, setData] = useState([]);  
+  const toastTopCenter = useRef(null);
+  const fileName = "Names.json";
+
+  useEffect(() => {
+    const loadData = async () => {
+      Axios.post("http://localhost:3001/getData", { fileName }).then((response) => {
+        const result = response.data;
+        if (result.length !== 0) {
+          setData(result);
+        }
+      })
+    }
+    try {
+      loadData();
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  const sendData = async (writtenData) => {
+    if (writtenData.length) {
+      
+      try {
+        await Axios.post("http://localhost:3001/writeData", { fileName, writtenData })       
+      } catch (error) {
+        console.log(error);
+      };
+    };
+  };
+
+  const showMessage = (summary, detail) => {
+    toastTopCenter.current.show([
+      { severity: 'error', summary: summary, detail: detail, life: 3000 }
+    ])
+  }
+
+  const checkNames = () => {
+    const names = data.map((i) => {
+      const name = i.name
+      return name.toUpperCase()
+    })
+    if (names.includes(nameEntered.toUpperCase())) {
+      setButtonClicked(true);
+      return true;
+    } else {
+      setButtonClicked(false)
+      showMessage("you're not on the list", "Hey, a lotta people's girlfriends are in there")
+      return false;
+    }
+  }
+
+  const checkDrawn = () => {
+    const index = data.findIndex((obj) => obj.name.toUpperCase() === nameEntered.toUpperCase())
+    if (data[index].drawn === true) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  const drawName = () => {
+    const names = data.map((i) => {
+      if (nameEntered.toUpperCase() === i.name.toUpperCase()) {
+        return null;
+      }
+      if (i.pickedBy !== "") {
+        return null;
+      }
+      else {
+        return i
+      }
+    })
+    const modNames = names.filter((element) => element !== null)
+    const randomName = Math.floor(Math.random() * modNames.length);
+    return modNames[randomName];
+  }
+
+  const handleButtonClick = async () => {
+    if (nameEntered.length === 0) {
+      showMessage('Sorry', "I can't let you through")
+    } else {
+      if (checkNames()) {
+        if (checkDrawn()) {
+          const randomName = drawName();
+          const arr = data;
+          const index = arr.findIndex((obj) => obj.name === randomName.name);
+          arr[index].pickedBy = nameEntered;
+
+          const index2 = arr.findIndex((obj) => obj.name.toUpperCase() === nameEntered.toUpperCase());
+          arr[index2].drawn = true;
+
+          const writtenData = arr
+          setPulledName(randomName.name)
+          setTimeout(() => {setIsPulledNameVisible(true);}, 2000)
+          sendData(writtenData)
+
+        } else {
+          showMessage(`I'm sorry, ${nameEntered}`, "I can't let you do that.")
+        }
+      };
+    }
+  };
+
+  const showPulledName = () => {
+    if (isPulledNameVisible) {
+      return (
+        <p className="pulled-name">{pulledName}</p>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <div>
+      <Toast ref={toastTopCenter} position="top-center" />
+      <div className="p-inputgroup">
+        <InputText
+          placeholder="Enter Name"
+          onChange={(e) => setNameEntered(e.target.value)}
+        />
+      </div>
+      <div className="center-container">
+        <div className="buttonContainer">
+          <Button
+            className={`drawButton ${buttonClicked ? 'clicked' : ''}`}
+            onClick={handleButtonClick}
+          >
+            Draw Name
+          </Button>
+          <div className="fade-box">
+            {showPulledName()} {/* Render the pulledName */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+const util = require('util');
+
+const app = express();
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+app.post("/getData", async (req, res) => {
+    const path = './data/JSON/' + req.body.fileName;
+    const buff = await readFile(path);
+    let content = buff.toString();
+    const obj = JSON.parse(content);
+    res.send(obj);
+});
+
+app.post("/writeData", async (req, res) => {
+    const fileName = req.body.fileName;
+    const path = './data/JSON/' + fileName;
+    const data = JSON.stringify(req.body.writtenData);
+    writeFile(path, data);
+    res.send(data);
+})
+
+app.listen(process.env.PORT || 3001)
+console.log("listening on 3001")
